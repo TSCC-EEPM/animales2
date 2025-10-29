fccff85929ca7af3749e5264b57d290fad6bf760
America Alejandra Gracida Hernandez <amegra01@gmail.com>
2025-10-29 08:15:32 -0600

    Agregando animal Pez

--- animales.py
+++ animales.py
@@ -6,7 +6,8 @@ def mostrar_animales():
     animales = [
         "Perro",
         "Gato",
-        "Loro"
+        "Loro",
+        "Pez"
     ]
     for a in animales:
         print("Animal:", a)
